d31fd4b47e86ca595525abddae3312d91b9ed858
qnik <piotr-kowa@protonmail.com>
2021-05-03 16:35:24 +0200

    hash all the files recursively with md5 algo

---
+++ hash_all_the_files.py
@@ -0,0 +1,48 @@
+import os
+from hasher import md5sum
+
+class PathChanger:
+    def __init__(self, path):
+        self.__mainPath = os.getcwd()
+        os.chdir(path)
+
+    def __del__(self):
+        os.chdir(self.__mainPath)
+
+class FileWriter:
+    def __init__(self, file_name):
+        self.f = open(file_name, "w")
+
+    def write(self, content):
+        self.f.write(content)
+
+    def __del__(self):
+        self.f.close()
+
+def create_check_sum_file_for_dir(files, dirpath):
+    full_md5_file_path = os.path.join(dirpath, os.path.basename(dirpath) + "_checksum.md5")
+    print("creating: " + full_md5_file_path + " for " + str(len(files)) + " files")
+    md5_sum_file = FileWriter(full_md5_file_path);
+    for filename in files:
+        full_file_path = os.path.join(dirpath, filename)
+        md5_sum = md5sum(full_file_path)
+        md5_sum_file.write(md5_sum + " " + filename + "\n")
+
+
+def do_for_dir(files, dirpath):
+    filter_out_md5_files = lambda f: f[-4:] != ".md5"
+    filtered_files = list(filter(filter_out_md5_files, files))
+    if len(filtered_files) > 0:
+        create_check_sum_file_for_dir(filtered_files, dirpath)
+
+
+
+def do_for_dir_recursively(directory):
+    path_changer = PathChanger(directory)
+    current_dir_rel_path = ".\\"
+    for dirpath, dirs, files in os.walk(current_dir_rel_path):
+        do_for_dir(files, dirpath)
+
+
+test_path = "./"
+do_for_dir_recursively(test_path)
---
+++ hasher.py
@@ -0,0 +1,18 @@
+import io
+import hashlib
+import zlib
+
+def md5sum(src, length=io.DEFAULT_BUFFER_SIZE):
+    md5 = hashlib.md5()
+    with io.open(src, mode="rb") as fd:
+        for chunk in iter(lambda: fd.read(length), b''):
+            md5.update(chunk)
+    return md5.hexdigest()
+
+
+def crc32(src, length=io.DEFAULT_BUFFER_SIZE):
+    crc_sum = 0
+    with io.open(src, mode="rb") as fd:
+        for chunk in iter(lambda: fd.read(length), b''):
+            crc_sum = zlib.crc32(chunk, crc_sum)
+    return format(crc_sum, "x").zfill(8)
